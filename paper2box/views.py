b80489a3857ca7a94c89038d292acf67c8c7480a
sdoxl <g.lstuma@gmail.com>
2023-02-04 15:04:57 +0100

    file upload not working .. >:(

--- paper2box/views.py
+++ paper2box/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
+import random
 
 def index(request):
     return HttpResponse(render(request, 'index.html'))
@@ -10,9 +11,15 @@ def editor(request):
         return redirect('/')
 
     # Get file from request
-    file = request.FILES['image']
+    print(request)
+    fn = request.POST['image']
+    file = request.FILES[fn]
 
-    path = ''.join([chr(random.range(65, 90)) for i in range(30)])+'.jpg'
+    # Local filepath
+    path = str()
+    for i in range(30): path += chr(random.randrange(65, 90))
+    path += '.jpg'
+
     with open('./media/img/'+path, 'w') as f:
         content = file.read()
         print(content)
